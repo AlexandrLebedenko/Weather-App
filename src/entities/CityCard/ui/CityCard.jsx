52158d5b2aa07a497e4229e9303f87de7f8fa670
AlexandrLebedenko <sasha.lebedenko.2012@gmail.com>
2026-07-13 13:54:53 +0300

    feat: add CityCard component

---
+++ src/entities/CityCard/ui/CityCard.jsx
@@ -0,0 +1,17 @@
+import styles from "./CityCard.module.scss";
+function CityCard({ country, city, condition, icon, temp }) {
+  return (
+    <div className={styles.cityCard}>
+      <div className={styles["cityCard__location"]}>
+        <span className={styles["cityCard__location-country"]}>{country}</span>
+        <p className={styles["cityCard__location-city"]}>{city}</p>
+        <p className={styles["cityCard__location-city-condition"]}>{condition}</p>
+      </div>
+      <div className={styles["cityCard__condition"]}>
+        <img className={styles["cityCard__condition-icon"]} src={icon} alt="" aria-hidden="true"></img>
+        <p className={styles["cityCard__condition-temp"]}>{temp}</p>
+      </div>
+    </div>
+  );
+}
+export default CityCard;
