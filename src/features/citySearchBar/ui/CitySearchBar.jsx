50b9830920649d0e8dd3813fff50742166b0c473
AlexandrLebedenko <sasha.lebedenko.2012@gmail.com>
2026-07-16 16:27:16 +0300

    feat: add searchbar component

--- src/features/citySearchBar/ui/CitySearchBar.jsx
+++ src/features/citySearchBar/ui/CitySearchBar.jsx
@@ -1,3 +1,11 @@
 import styles from "./CitySearchBar.module.scss";
-function CitySearchBar() {}
+import searchIcon from "@/assets/icons/Search.svg";
+function CitySearchBar({ value, onChange }) {
+  return (
+    <label className={styles.searchbar}>
+      <img src={searchIcon} alt="search icon" />
+      <input type="text" name="searchbar" id="searchbar" placeholder="Search city...." value={value} onChange={(e) => onChange(e.target.value)} />
+    </label>
+  );
+}
 export default CitySearchBar;
